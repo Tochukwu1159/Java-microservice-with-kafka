8ff9ed692d3532d86606c56e2dcd6e7a79b726a6
Tochukwu1159 <christinotochukwu@gmail.com>
2023-03-10 21:20:37 +0100

    project completed

---
+++ base-domains/src/test/java/com/kafkamicroservice/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkamicroservice.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ email-service/src/test/java/com/kafkamicroservice/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkamicroservice.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
